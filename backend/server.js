ed8549cf14363c2ae8668c2ab7b3daf2039229f1
Deepavishali <deepavishalig@gmail.com>
2023-05-24 14:33:06 +0530

    cors applied

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
-
-
+const cors = require("cors");
 
 
 dotenv.config();
@@ -14,6 +13,7 @@ connectDB();
 const app = express();
 
 app.use(express.json()); // to accept json data
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("API Running!");
